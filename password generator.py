264546d6f1dcaf0c26452bab60489a79df4cc1d6
habeebsait <150616498+habeebsait@users.noreply.github.com>
2024-05-12 01:43:54 +0530

    password generator.py

---
+++ password generator.py
@@ -0,0 +1,31 @@
+#password generator
+
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+password = []
+
+a = input("enter the number of alphabets needed: ")
+n = input("Enter the number of numbers needed: ")
+s = input("Enter the numbers of symbols needed: ")
+
+for char in range(1,int(a)+1):
+    randomchar = random.choice(letters)
+    password += randomchar
+
+for char in range(1,int(n)+1):
+    randomchar = random.choice(numbers)
+    password += randomchar
+
+for char in range(1,int(s)+1):
+    randomchar = random.choice(symbols)
+    password += randomchar
+
+random.shuffle(password)
+
+
+for char in password:
+    print(char, end = "")
