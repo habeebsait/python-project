1a9b9098e038a7ef6580fc15008e8ceb1262a525
MD Habeeb Sait <150616498+habeebsait@users.noreply.github.com>
2024-10-01 04:28:59 +0530

    Update password_manager.py

--- Password_manager/password_manager.py
+++ Password_manager/password_manager.py
@@ -4,7 +4,37 @@ import random
 import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def random_pass():
+    pass_entry.delete(0,END)
 
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password = []
+
+    a=random.randint(2,4)
+    n=random.randint(2,4)
+    s=random.randint(2,4)
+
+    for char in range(1,int(a)+1):
+        randomchar = random.choice(letters)
+        password += randomchar
+
+    for char in range(1,int(n)+1):
+        randomchar = random.choice(numbers)
+        password += randomchar
+
+    for char in range(1,int(s)+1):
+        randomchar = random.choice(symbols)
+        password += randomchar
+
+
+    random.shuffle(password)
+    strong_pass = "".join(password)
+    
+
+    pass_entry.insert(0,f"{strong_pass}")
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def save_file():
@@ -41,37 +71,7 @@ def save_file():
     pass_entry.delete(0,END)
 
 
-def random_pass():
-    pass_entry.delete(0,END)
 
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
-    password = []
-
-    a=random.randint(2,4)
-    n=random.randint(2,4)
-    s=random.randint(2,4)
-
-    for char in range(1,int(a)+1):
-        randomchar = random.choice(letters)
-        password += randomchar
-
-    for char in range(1,int(n)+1):
-        randomchar = random.choice(numbers)
-        password += randomchar
-
-    for char in range(1,int(s)+1):
-        randomchar = random.choice(symbols)
-        password += randomchar
-
-
-    random.shuffle(password)
-    strong_pass = "".join(password)
-    
-
-    pass_entry.insert(0,f"{strong_pass}")
 
 
 def search():
