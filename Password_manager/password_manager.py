08108e7e1e740626308f4f5c95d815742e9a516f
MD Habeeb Sait <150616498+habeebsait@users.noreply.github.com>
2024-09-30 18:13:19 +0530

    Update password_manager.py
    search button was added

--- Password_manager/password_manager.py
+++ Password_manager/password_manager.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
 import random
-
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -11,23 +11,34 @@ def save_file():
     web= website_entry.get()
     mail = email_entry.get()
     passw = pass_entry.get()
-    
+    new_data = {
+        web: {
+        "email" : mail,
+        "password" : passw,
+    }
+    }
     if len(web) ==0 or len(mail)==0 or len(passw) ==0:
         messagebox.showerror(title="Warning!!", message= "Fields are empty")
         fields = False
     
     else:
-        warning = messagebox.askokcancel(title="Title", message= f"These are the details entered: \nWebsite: {web}\nEmail: {mail}\nPassword:{passw}\nIs it ok to save?")
+            try:
+                with open("day 29, password gui/data.json","r") as f:
+                    # json.dump(new_data, f , indent = 4)
+                    data =json.load(f)
+                    data.update(new_data)
+                
+                
 
-    
+            except:
+                with open("day 29, password gui/data.json","w") as f:
+                    json.dump(new_data,f, indent =4)
 
-        if warning == True:
-            with open("day 29, password gui/data.txt","a") as f:
-                f.write(f"{web} | {mail} | {passw}")
-                f.write("\n")
-
-            website_entry.delete(0, END)
-            pass_entry.delete(0,END)
+            else:
+                with open("day 29, password gui/data.json","w") as f:
+                    json.dump(data,f, indent =4)
+    website_entry.delete(0, END)
+    pass_entry.delete(0,END)
 
 
 def random_pass():
@@ -62,6 +73,18 @@ def random_pass():
 
     pass_entry.insert(0,f"{strong_pass}")
 
+
+def search():
+    web= website_entry.get()
+    try:
+        with open("day 29, password gui/data.json") as f:
+            data = json.load(f)
+            mail = data[web]["email"]
+            passw = data[web]["password"]
+            messagebox.showinfo(message = f"Website: {web}\nEmail: {mail}\nPassword: {passw}")
+
+    except KeyError:
+        messagebox.showinfo(title= "Error", message = f"Website {web} not found")
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.minsize(500,400)
@@ -82,15 +105,15 @@ pass_label.grid(row=3, column=0)
 
 
 
-website_entry = Entry(width =35)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry = Entry(width =21)
+website_entry.place(x=120, y=206)
 website_entry.focus()
 
 email_entry = Entry(width =35)
 email_entry.grid(column=1,row=2, columnspan=2)
 email_entry.insert(0,"habeeb@gmail.com")
 
-pass_entry = Entry(width = 21, show = "*")
+pass_entry = Entry(width = 21)
 pass_entry.place(x=120,y=263)
 
 
@@ -101,6 +124,17 @@ gen_button.grid(column=2, row= 3)
 add_button = Button(text = "Add",command = save_file)
 add_button.grid(row=4, column=1, columnspan=2)
 add_button.config(width = 35)
+
+search_button = Button(text = "Search", command = search )
+search_button.grid(column=2, row=1)
+
+
+
+
+
+
+
+
 
 
 
